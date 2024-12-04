293670980fc47e48dfadb14d521c9d0c1296eaca
caleb <musyokacaleb5@gmail.com>
2024-12-04 08:56:12 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  // Arrow function for square
+  const square = (x) => x * x;
+  
+  // Arrow function for add
+  const add = (a, b) => a + b;
+  
+  // Exporting the functions for testing
+  module.exports = {
+    divide,
+    square,
+    add
+  };
+  
